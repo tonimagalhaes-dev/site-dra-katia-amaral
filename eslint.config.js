bc709e533033d23ee21c185d73bec4eaa71a84c6
tonimagalhaes.dev <tonimagalhaes.dev@gmail.com>
2025-07-25 12:38:18 -0300

    ajuste lints

--- eslint.config.js
+++ eslint.config.js
@@ -2,18 +2,22 @@ import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 
-export default tseslint.config(
-  { ignores: ["dist"] },
+export default [
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    ignores: ["dist"],
+  },
+  {
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
+      parser: tsParser,
       globals: globals.browser,
     },
     plugins: {
+      "@typescript-eslint": tseslint,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
     },
@@ -25,5 +29,6 @@ export default tseslint.config(
       ],
       "@typescript-eslint/no-unused-vars": "off",
     },
-  }
-);
+    extends: [js.configs.recommended],
+  },
+];
